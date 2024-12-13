621f3576624c501ea765f96700b64559593aea9e
Lubna2302 <139913239+Lubna2302@users.noreply.github.com>
2024-12-13 12:03:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // src/index.js
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // React 18 import
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom'; // Correct import for Router
+import './index.css';
 
-// Create the root and render the App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
